016adfe8fff7aa026d7ecd0ac06748ee3068767b
Budhaoo <mohdamaan998@gmail.com>
2025-04-13 01:46:39 +0530

    Added

---
+++ main.py
@@ -0,0 +1,60 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+import musiclibrary
+import requests
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+
+def speak(text):
+    # Converts text to speech.
+    engine.say(text)
+    engine.runAndWait()
+    # newsapi=0f6633b54505466bbc12db256f8616bf
+def processCommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://facebook.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+    elif "open linkedin" in c.lower():
+        webbrowser.open("https://linkedin.com")
+
+    elif c.lower().startswith("play"):
+        song = c.lower().replace("play", "").strip()
+        try:
+            link = musiclibrary.music[song]
+            webbrowser.open(link)
+        except KeyError:
+            speak(f"Sorry, I couldn't find the song {song}.")
+
+   
+
+
+if __name__=="__main__":
+    speak("Initializing...")
+    while True:
+        # Listen for the wake word
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+
+        print("Recognizing...")
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source, timeout=2, phrase_time_limit=1)
+            word = r.recognize_google(audio)
+            if(word.lower() == "python"):
+                speak("its on...")
+                # Listen for command
+                with sr.Microphone() as source:
+                    print("Yup...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+                    processCommand(command)
+
+
+        except Exception as e:
+            print("Error; {0}".format(e))
---
+++ musiclibrary.py
@@ -0,0 +1,11 @@
+music = {
+    "blinding lights": "https://www.youtube.com/watch?v=4NRXx6U8ABQ",
+    "shape of you": "https://www.youtube.com/watch?v=JGwWNGJdvx8",
+    "someone like you": "https://www.youtube.com/watch?v=hLQl3WQQoQ0",
+    "let her go": "https://www.youtube.com/watch?v=RBumgq5yVrA",
+    "perfect": "https://www.youtube.com/watch?v=2Vv-BfVoq4g",
+    "see you again": "https://www.youtube.com/watch?v=RgKAFK5djSk",
+    "love me like you do": "https://www.youtube.com/watch?v=AJtDXIazrMo",
+    "photograph": "https://www.youtube.com/watch?v=nSDgHBxUbVQ",
+    "night changes": "https://www.youtube.com/watch?v=syFZfO_wfMQ"
+}
